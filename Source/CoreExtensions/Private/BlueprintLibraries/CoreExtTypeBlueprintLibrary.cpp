2da86e66a32b59e0196b524b366f822a32131e74
Hades Fury <hadesfury@gmail.com>
2024-11-21 11:54:02 +0100

    [DEV] Add Lowest and Max float values for blueprint

---
+++ Source/CoreExtensions/Private/BlueprintLibraries/CoreExtTypeBlueprintLibrary.cpp
@@ -0,0 +1,11 @@
+﻿#include "BlueprintLibraries/CoreExtTypeBlueprintLibrary.h"
+
+float UCoreExtTypeBlueprintLibrary::LowestFloatValue()
+{
+    return TNumericLimits< float >().Lowest();
+}
+
+float UCoreExtTypeBlueprintLibrary::MaxFloatValue()
+{
+    return TNumericLimits< float >().Max();
+}
---
+++ Source/CoreExtensions/Public/BlueprintLibraries/CoreExtTypeBlueprintLibrary.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreExtTypeBlueprintLibrary.generated.h"
+
+UCLASS()
+class COREEXTENSIONS_API UCoreExtTypeBlueprintLibrary final : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+public:
+    UFUNCTION( BlueprintPure )
+    static float LowestFloatValue();
+    UFUNCTION( BlueprintPure )
+    static float MaxFloatValue();
+};
